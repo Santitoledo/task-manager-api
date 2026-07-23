175da03a4138cb9ce81977f697ec2b45142eeac4
Santiago Toledo <santi.toledo1974@gmail.com>
2026-07-23 19:14:03 -0300

    feat: improve GET and POST error handling

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,43 +1,88 @@
 import { Router } from "express";
 import { prisma } from "../lib/prisma";
+import { Prisma } from "@prisma/client";
 
 const router = Router();
 
 
 router.get("/", async (req, res) => {
-  const users = await prisma.user.findMany();
+  try {
+    const users = await prisma.user.findMany();
 
-  res.json(users);
+    return res.json(users);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 });
 
 router.get("/:id", async (req, res) => {
   const id = Number(req.params.id);
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id,
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Usuario no encontrado",
+      });
+    }
+
+    return res.json(user);
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
+ 
+
+router.post("/", async (req, res) => {
+const { name, email } = req.body;
+
+if (!name) {
+  return res.status(400).json({
+    message: "Name is required",
+  });
+}
+
+if (!email) {
+  return res.status(400).json({
+    message: "Email is required",
+  });
+}
+
+try {
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      email,
     },
   });
 
-  if (!user) {
-    return res.status(404).json({
-      message: "Usuario no encontrado",
+  return res.status(201).json(newUser);
+
+} catch (error) {
+  if (
+    error instanceof Prisma.PrismaClientKnownRequestError &&
+    error.code === "P2002"
+  ) {
+    return res.status(409).json({
+      message: "Email already exists",
     });
   }
 
-  res.json(user);
-});
-
-router.post("/", async (req, res) => {
-  const newUser = await prisma.user.create({
-    data: {
-      name: req.body.name,
-      email: req.body.email,
-    },
+  return res.status(500).json({
+    message: "Internal server error",
   });
-
-  res.status(201).json(newUser);
-});
+}
+ });
 
 router.put("/:id", async (req, res) => {
   const id = Number(req.params.id);
