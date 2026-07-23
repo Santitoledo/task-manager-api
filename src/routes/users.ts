59b9dfb04177755797cdc8f1548b670f115cf33d
Santiago Toledo <santi.toledo1974@gmail.com>
2026-07-23 14:51:53 -0300

    feat: complete user CRUD with Prisma

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,18 +3,12 @@ import { prisma } from "../lib/prisma";
 
 const router = Router();
 
-const users = [
-  {
-    id: 1,
-    name: "Santiago",
-    email: "santi@gmail.com",
-  },
-  {
-    id: 2,
-    name: "Juan",
-    email: "juan@gmail.com",
-  },
-];
+
+router.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  res.json(users);
+});
 
 router.get("/:id", async (req, res) => {
   const id = Number(req.params.id);
@@ -43,6 +37,60 @@ router.post("/", async (req, res) => {
   });
 
   res.status(201).json(newUser);
+});
+
+router.put("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "Usuario no encontrado",
+    });
+  }
+
+  const updatedUser = await prisma.user.update({
+    where: {
+      id,
+    },
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+    },
+  });
+
+  res.json(updatedUser);
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "Usuario no encontrado",
+    });
+  }
+
+  const deletedUser = await prisma.user.delete({
+    where: {
+      id,
+    },
+  });
+
+  return res.json({
+  message: "Usuario eliminado correctamente",
+});
 });
 
 export default router;
